b047e588f173941078e44d5b6961d2d6b937d91c
이상원 <sangwon020214@gmail.com>
2025-11-23 05:44:47 +0900

    feat: 로그인/회원가입 및 오퍼 관련 api 예외 처리

---
+++ src/main/java/com/enomdeul/domain/offer/exception/OfferException.java
@@ -0,0 +1,14 @@
+package com.enomdeul.domain.offer.exception;
+
+import com.enomdeul.global.common.response.code.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class OfferException extends RuntimeException {
+    private final BaseErrorCode errorCode;
+
+    public OfferException(BaseErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
--- src/main/java/com/enomdeul/domain/user/dto/request/UserSignupReq.java
+++ src/main/java/com/enomdeul/domain/user/dto/request/UserSignupReq.java
@@ -1,12 +1,21 @@
 package com.enomdeul.domain.user.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UserSignupReq {
+
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
     private String loginId;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.") // 이메일 형식 검증 추가
     private String email;
 }

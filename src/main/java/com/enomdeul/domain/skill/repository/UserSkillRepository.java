943cb73b333f267b583a00a828d51e246f9fdc71
hajimeong <ha_01@naver.com>
2025-11-23 04:41:24 +0900

    feat: 스킬 id로 userSkill를 조회 추가

--- src/main/java/com/enomdeul/domain/skill/repository/UserSkillRepository.java
+++ src/main/java/com/enomdeul/domain/skill/repository/UserSkillRepository.java
@@ -1,7 +1,11 @@
 package com.enomdeul.domain.skill.repository;
 
 import com.enomdeul.domain.skill.entity.UserSkill;
+import com.enomdeul.domain.skill.entity.UserSkillId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
+import java.util.List;
+
+public interface UserSkillRepository extends JpaRepository<UserSkill, UserSkillId> {
+    List<UserSkill> findBySkill_SkillId(Long skillId);
 }

fda2a9df451bac9d086a8f42b05e9c9e56d0371f
hajimeong <ha_01@naver.com>
2025-11-23 06:51:16 +0900

    feat: 해당 UserId의 Skill 리스트 조회 쿼리문 추가

--- src/main/java/com/enomdeul/domain/skill/repository/UserSkillRepository.java
+++ src/main/java/com/enomdeul/domain/skill/repository/UserSkillRepository.java
@@ -1,13 +1,19 @@
 package com.enomdeul.domain.skill.repository;
 
+import com.enomdeul.domain.skill.entity.Skill;
 import com.enomdeul.domain.skill.entity.UserSkill;
 import com.enomdeul.domain.skill.entity.UserSkillId;
 import com.enomdeul.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserSkillRepository extends JpaRepository<UserSkill, UserSkillId> {
     List<UserSkill> findBySkill_SkillId(Long skillId);
     List<UserSkill> findAllByUser(User user);
+
+    @Query("SELECT us.skill FROM UserSkill us WHERE us.user.userId = :userId")
+    List<Skill> findSkillsByUserId(@Param("userId") Long userId);
 }

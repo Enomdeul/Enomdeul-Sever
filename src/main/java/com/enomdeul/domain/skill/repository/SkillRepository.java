119d4d11099244fbfae1a636402f01e7e878a9cd
hajimeong <ha_01@naver.com>
2025-11-23 02:52:41 +0900

    feat: Skill 직군별 스킬 조회 기능 추가

---
+++ src/main/java/com/enomdeul/domain/skill/repository/SkillRepository.java
@@ -0,0 +1,10 @@
+package com.enomdeul.domain.skill.repository;
+
+import com.enomdeul.domain.skill.entity.Skill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SkillRepository extends JpaRepository<Skill, Integer> {
+    List<Skill> findByJobGroup(String jobGroup);
+}
